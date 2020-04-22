3c39acccc7755478c040807fb567c379e031201c
shawn220a <shawn.tschoepe@icloud.com>
2020-04-21 22:27:27 -0500

    Print employees on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Table from './components/Table';
+import axios from 'axios';
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+
+  const [filterText, setFilterText] = useState('');
+  const employeeSort = employees.filter((e) => {
+    if(e.name.first.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+    else if(e.name.last.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+    else if(e.email.toLowerCase().includes(filterText.toLowerCase()) ) {
+      return true;
+    }
+    else {
+      return false
+    }
+  })
+
+  useEffect(() => {
+    axios
+      .get('https://randomuser.me/api/?results=25&nat=us')
+      .then(function (response) {
+        setEmployees(response.data.results);
+      })
+      .catch(function (error) {})
+  }, []);
+
   return (
     <div>
       <Header />
-      {/* <EmployeeTable /> */}
-      <Table />
+      <Table employees={employeeSort}/>
     </div>
   );
 }
--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -3,34 +3,26 @@ import { Table } from 'reactstrap';
 
 const Example = (props) => {
   return (
-    <Table dark>
+    <Table >
       <thead>
         <tr>
-          <th>#</th>
+          <th>Profile Picture</th>
           <th>First Name</th>
           <th>Last Name</th>
           <th>Email</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
-        </tr>
+      {props.employees.map((e) => {
+          return (<tr key={e.id.value}>    
+          <td><img src={e.picture.thumbnail} alt="employee"/> </td>
+          <td>{e.name.first}</td>
+          <td>{e.name.last}</td>
+          <td>{e.email}</td>
+      
+          </tr>)
+
+        })}
       </tbody>
     </Table>
   );
